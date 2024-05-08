730aedc271f81d37d7c0f635d11ea0b88d7f1f20
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-05-08 23:10:07 +0000

    feat: :sparkles: Agregar detección del switch para modo claro y oscuro

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let light_mode = document.getElementById("switch")
+light_mode.addEventListener("change",function(){
+    if(this.checked){
+        console.log("Check")
+    } else {
+        console.log("No check")
+    }
+})
+
