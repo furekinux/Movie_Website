dbb429258019248c89dab088f043377ae1b7fe43
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-05-09 06:58:02 -0500

    feat: :sparkles: Permitir modo claro y oscuro

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ let light_mode = document.getElementById("switch")
 light_mode.addEventListener("change",function(){
     if(this.checked){
         console.log("Check")
+        document.getElementById('information').style.backgroundColor = '#000000'
+
     } else {
         console.log("No check")
+        document.getElementById('information').style.backgroundColor = '#ffffff'
     }
 })
 
