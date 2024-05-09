29312cec8a0c9225dd135e486615d233578055df
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-05-09 20:10:50 +0000

    feat: :sparkles: Agregar fetch para películas

--- js/script.js
+++ js/script.js
@@ -2,11 +2,36 @@ let light_mode = document.getElementById("switch")
 light_mode.addEventListener("change",function(){
     if(this.checked){
         console.log("Check")
-        document.getElementById('information').style.backgroundColor = '#000000'
-
+        document.getElementById('information').style.backgroundColor = '#121212'
+        document.getElementById('information').style.color = '#ffffff'
     } else {
         console.log("No check")
-        document.getElementById('information').style.backgroundColor = '#ffffff'
+        document.getElementById('information').style.backgroundColor = '#fafafa'
+        document.getElementById('information').style.color = '#121212'
     }
 })
 
+searchArray=[]
+
+function searchMovie(){
+    let search = document.getElementById("search_box").value
+    let url_streaming =`https://search.imdbot.workers.dev/?q=${search}`
+    let xhr = new XMLHttpRequest()
+    xhr.open("GET",url_streaming,true);
+    xhr.onreadystatechange = function(){
+        if (this.readyState === 4 && this.status === 200){
+            let streaming = JSON.parse(this.responseText)
+            console.log(streaming)
+            searchArray=streaming.description
+            console.log(searchArray)
+            searchArray.forEach(movie => {
+                console.log(movie)
+            });
+        } else if(this.readyState === 4){
+            console.log("Error :(",this.statusText)
+        }
+    }
+    xhr.send();
+}
+//https://www.w3schools.com/howto/howto_css_searchbar.asp
+//https://justwatch.imdbot.workers.dev/?q=Niram&L=en_IN
